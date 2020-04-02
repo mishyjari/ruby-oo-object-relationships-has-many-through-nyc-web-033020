f02128d48cca94f927d8b04c5ee4653d3aaf7581
mishyjari <mishyjari@outlook.com>
2020-04-02 18:21:12 -0400

    all tests passing

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,3 +1,23 @@
 class Customer
-  
+	@@all = []
+	attr_accessor :name, :age
+	def initialize (name,age)
+		@name = name
+		@age = age
+		Customer.all << self
+	end
+	def self.all
+		@@all
+	end
+	def new_meal(waiter,total,tip)
+		Meal.new(waiter,self,total,tip)
+	end
+	def meals
+		Meal.all.select{ |meal| meal.customer == self }
+	end
+	def waiters
+		Waiter.all.select do | waiter |
+			self.meals.any?{ |meal| meal.waiter == waiter}
+		end
+	end
 end
--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,21 @@
 class Waiter
-
+	@@all = []
+	attr_accessor :name, :experience
+	def initialize (name,experience)
+		@name = name
+		@experience = experience
+		Waiter.all << self
+	end
+	def self.all
+		@@all
+	end
+	def new_meal(customer,total,tip)
+		Meal.new(self,customer,total,tip)
+	end
+	def meals
+		Meal.all.select{ |meal| meal.waiter == self }
+	end
+	def best_tipper
+		Meal.all.max{ |a,b| a.tip <=> b.tip }.customer
+	end
 end
